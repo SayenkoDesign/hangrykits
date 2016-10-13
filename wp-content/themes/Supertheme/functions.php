17738fe80f1e7ddc01b1cda973af05ce761ab9a9
Robert M. Parker <rparker@yamiko.ninja>
2016-10-13 16:54:36 -0700

    add shop url as global context

--- wp-content/themes/Supertheme/functions.php
+++ wp-content/themes/Supertheme/functions.php
@@ -1,3 +1,9 @@
 <?php
 require_once __DIR__.'/app/bootstrap.php';
 require_once __DIR__.'/src/functions.php';
+
+add_filter('timber/context', function($data){
+    $data['favicon'] = get_field('favicon', 'option') ?: get_field('mobile_logo', 'option');
+    $data['shop_url'] = get_permalink(wc_get_page_id('shop'));
+    return $data;
+});
